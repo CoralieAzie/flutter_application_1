77df2b6ce9ff3ffc4950a5a1cb8dab2e51c8a36a
Coralie Azie <coralieazie974@gmail.com>
2022-03-09 15:34:32 +0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/storybrain.dart';
+
+StoryBrain sb = StoryBrain();
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: Text("Ti zistoire la kour"),
+      ),
+      body: HomeApp(),
+    ),
+  ));
+}
+
+class HomeApp extends StatefulWidget {
+  const HomeApp({Key? key}) : super(key: key);
+
+  @override
+  State<HomeApp> createState() => _HomeAppState();
+}
+
+class _HomeAppState extends State<HomeApp> {
+  get visible => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 4,
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Center(
+              child: Text(
+                sb.getQuestion(),
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.green)),
+              onPressed: () {
+                setState(() {
+                  sb.suivant(1);
+                });
+              },
+              child:
+                  Text(sb.getChoice1(), style: TextStyle(color: Colors.white)),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Visibility(
+              visible: sb.buttonVisibleorNot(),
+              child: TextButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.red)),
+                onPressed: () {
+                  setState(() {
+                    sb.suivant(2);
+                  });
+                },
+                child: Text(sb.getChoice2(),
+                    style: TextStyle(color: Colors.white)),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
